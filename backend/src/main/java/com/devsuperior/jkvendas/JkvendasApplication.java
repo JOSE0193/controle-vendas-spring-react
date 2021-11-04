502bca5f8e11f58e80951de4be549f771838b853
Jose Antonio <jose01.11.93@gmail.com>
2021-11-04 14:54:23 -0300

    Criação do projeto backend e frontend

---
+++ backend/src/main/java/com/devsuperior/jkvendas/JkvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.jkvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkvendasApplication.class, args);
+	}
+
+}
